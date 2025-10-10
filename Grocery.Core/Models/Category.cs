da9ca3562e9fc6c251bafff7e3d650f456e9af77
yehoshuaa <jozuab@hotmail.com>
2025-10-10 18:54:33 +0200

    fix

---
+++ Grocery.Core/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.Core.Models
+{
+    public partial class Category : Model
+    {
+        public Category(int id, string name) : base(id, name) { }
+        public override string ToString() => Name;
+    }
+}
